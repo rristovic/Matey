de8df2414ae5645d54c5a626bd294e03abf838e5
Radovan Ristovic <radovanr995@gmail.com>
2016-05-11 17:20:05 +0200

    Implemented GCM services using google dev guide @https://developers.google.com/cloud-messaging/android/client

---
+++ app/src/main/java/com/mateyinc/marko/matey/gcm/MateyGCMPreferences.java
@@ -0,0 +1,11 @@
+package com.mateyinc.marko.matey.gcm;
+
+/**
+ * Created by Sarma on 5/11/2016.
+ */
+public class MateyGCMPreferences {
+
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+
+}
