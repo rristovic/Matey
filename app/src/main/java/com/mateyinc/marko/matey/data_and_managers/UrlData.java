6a8fcdce21075c450b4bd0112507870bedf94b7f
M4rko <marko.ognjenovicgm@gmail.com>
2016-05-13 00:54:46 +0200

    Novi URL-ovi jer je api prebacen.

--- app/src/main/java/com/mateyinc/marko/matey/data_and_managers/UrlData.java
+++ app/src/main/java/com/mateyinc/marko/matey/data_and_managers/UrlData.java
@@ -7,13 +7,13 @@ import android.os.Build;
  */
 public class UrlData {
 
-    public final static String FIRST_RUN_URL = "http://notifinda-api-m4rk07.c9users.io/web/index.php/api/user/firstRun/" +
+    public final static String FIRST_RUN_URL = "http://notifinda-api-m4rk07-1.c9users.io/web/index.php/api/user/firstRun/" +
             Build.BOARD + "/" + Build.BRAND + "/" + Build.DEVICE + "/" + Build.MODEL;
-    public final static String REGISTER_URL = "http://notifinda-api-m4rk07.c9users.io/web/index.php/api/user/register";
-    public final static String LOG_URL = "http://notifinda-api-m4rk07.c9users.io/web/index.php/api/user/login";
-    public final static String LOG_OUT_URL = "http://notifinda-api-m4rk07.c9users.io/web/index.php/api/user/logout";
-    public final static String FACEBOOK_LOG_URL = "http://notifinda-api-m4rk07.c9users.io/web/index.php/api/user/fblogin";
-    public final static String FETCH_ALL_POSTS = "http://notifinda-api-m4rk07.c9users.io/web/index.php/api/user/requestAllPosts";
+    public final static String REGISTER_URL = "http://notifinda-api-m4rk07-1.c9users.io/web/index.php/api/user/register";
+    public final static String LOG_URL = "http://notifinda-api-m4rk07-1.c9users.io/web/index.php/api/user/login";
+    public final static String LOG_OUT_URL = "http://notifinda-api-m4rk07-1.c9users.io/web/index.php/api/user/logout";
+    public final static String FACEBOOK_LOG_URL = "http://notifinda-api-m4rk07-1.c9users.io/web/index.php/api/user/fblogin";
+    public final static String FETCH_ALL_POSTS = "http://notifinda-api-m4rk07-1.c9users.io/web/index.php/api/user/requestAllPosts";
 
     public final static String CHECK_USER_URL = "http://notifinda-api-m4rk07.c9users.io/web/index.php/api/user/firstRun";
 
